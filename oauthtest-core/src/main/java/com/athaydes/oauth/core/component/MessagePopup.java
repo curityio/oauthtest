5565e50c1d90a1788f07124ad99638fc23801fd7
renato <renato@twobo.com>
2016-11-03 08:40:46 +0100

    Attempt to improve MessagePopup.

--- oauthtest-core/src/main/java/com/athaydes/oauth/core/component/MessagePopup.java
+++ oauthtest-core/src/main/java/com/athaydes/oauth/core/component/MessagePopup.java
@@ -7,7 +7,7 @@ import javafx.animation.Timeline;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -23,15 +23,19 @@ public class MessagePopup {
 
     private static final double WIDTH = 400;
     private static final double HEIGHT = 900;
+    private static final double BALOON_SPACING = 5;
 
-    private final VBox panels;
+    private final GridPane panels;
     private final Stage dialog;
 
     public MessagePopup( Stage primaryStage, EventBus eventBus ) {
         eventBus.subscribe( Notification.class, this::show );
 
-        this.panels = new VBox( 5 );
+        this.panels = new GridPane();
+        //panels.setFillWidth( false );
+        panels.setMaxWidth( WIDTH );
         panels.setStyle( "-fx-background-color: transparent; -fx-border-style: none;" );
+
         Scene dialogScene = new Scene( panels, WIDTH, HEIGHT, null );
 
         this.dialog = new Stage();
@@ -39,9 +43,6 @@ public class MessagePopup {
         dialog.initStyle( StageStyle.TRANSPARENT );
         dialog.initOwner( primaryStage );
         dialog.setScene( dialogScene );
-
-        // FIXME not working
-        dialog.maxHeightProperty().bind( panels.heightProperty().add( 30 ) );
     }
 
     private static String colorFor( Notification.Level level ) {
@@ -56,14 +57,17 @@ public class MessagePopup {
     }
 
     private void show( Notification notification ) {
+        // FIXME textPane still overflows the parent
         GridPane textPane = new GridPane();
         textPane.setStyle( "-fx-background-color: " + colorFor( notification.getLevel() ) + ";" +
                 "-fx-background-radius: 6;" +
                 "-fx-cursor: hand;" );
+        textPane.setMaxWidth( 30 );
 
         // hide the text within  this block to avoid accidental use
         {
             Text text = new Text( notification.getText() );
+            text.setWrappingWidth( WIDTH - 15 );
             textPane.getChildren().add( text );
             GridPane.setMargin( text, new Insets( 10 ) );
         }
@@ -79,6 +83,7 @@ public class MessagePopup {
             dialog.setY( y );
         }
 
+        adjustDialog();
         dialog.show();
 
         Runnable removeNotification = () -> {
@@ -86,6 +91,8 @@ public class MessagePopup {
                 panels.getChildren().remove( textPane );
                 if ( panels.getChildren().isEmpty() ) {
                     dialog.hide();
+                } else {
+                    adjustDialog();
                 }
             }
         };
@@ -98,6 +105,24 @@ public class MessagePopup {
 
             timeline.play();
         }
+    }
+
+    private void adjustDialog() {
+        // couldn't figure out a way to resize the dialog that did not involve waiting for the panels to resize first
+        Timeline timeline = new Timeline( new KeyFrame( Duration.millis( 50 ),
+                ( e ) -> {
+                    double totalHeight = panels.getChildren().stream()
+                            .mapToDouble( it -> ( ( Region ) it ).getHeight() )
+                            .sum();
+
+                    double spacingToAdd = panels.getChildren().size() == 1 ? 2D :
+                            panels.getChildren().size() * BALOON_SPACING + 2D;
+
+                    System.out.println( "Total height is " + totalHeight );
+                    dialog.setHeight( totalHeight + spacingToAdd );
+                } ) );
+
+        timeline.play();
     }
 
 }
