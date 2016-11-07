b8ffa7b07e2085005bfaa0f6fe104e249092d273
renato <renato@twobo.com>
2016-11-07 21:11:59 +0100

    Added favicon.png.

--- oauthtest-core/src/main/java/se/curity/oauth/core/MainApplication.java
+++ oauthtest-core/src/main/java/se/curity/oauth/core/MainApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.util.BuilderFactory;
 
@@ -32,6 +33,10 @@ public class MainApplication extends Application
                 new ApplicationContainer(primaryStage)::get);
 
         primaryStage.setScene(new Scene(oauthFlows, 600, 700));
+
+        primaryStage.getIcons().addAll(
+                new Image(getClass().getResourceAsStream("/images/favicon.png")));
+
         primaryStage.show();
     }
 }
